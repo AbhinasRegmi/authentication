85c61113520c67c926b5643f60d4fbf22bc1ea78
abhinas regmi <aviregmi2018@gmail.com>
2024-04-03 23:31:33 +0545

    fixed a typo

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
-import * as schema from "@/db/schema";
+import * as schema from "@/db/schemas";
 
 declare module global {
   let postgresSqlClient: ReturnType<typeof postgres> | undefined
