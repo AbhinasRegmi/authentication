29b0efdd5e0031b300988c63b95c1c3a1e7060b4
abhinas regmi <aviregmi2018@gmail.com>
2024-04-15 10:17:43 +0545

    protected, public routes created.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,45 @@
-import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+import {NextResponse} from "next/server";
+import {
+  publicRoutes,
+  authRoutes,
+  apiAuthPrefix,
+  DEFAULT_LOGIN_REDIRECT
+} from "@/routes";
 
 const {auth: middleware} = NextAuth(authConfig);
 
 export default middleware((req) => {
+  const {nextUrl} = req;
   const isLoggedIn = !!req.auth;
 
-  console.log(isLoggedIn);
+  const isApiAuthPrefixRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if(isApiAuthPrefixRoute){
+    return NextResponse.next();
+  }
+
+  if(isAuthRoute){
+    if(isLoggedIn){
+      return NextResponse.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT, req.url)
+      )
+    }
+
+    return NextResponse.next();
+  }
+
+  if(!isLoggedIn && !isPublicRoute){
+    return NextResponse.redirect(
+      new URL("/auth/login", req.url)
+    )
+  }
+
+  return NextResponse.next();
+
 })
 
 
