9029c9a535f56a60b624024f2308b27cf9b17851
abhinas regmi <aviregmi2018@gmail.com>
2024-04-17 22:55:38 +0545

    fixed a small bug

--- db/query/verification.ts
+++ db/query/verification.ts
@@ -11,5 +11,5 @@ export async function getVerificationTokenByEmail(email: string){
 export async function getUserEmailFromToken(token: string){
     const res = await db.select({email: verification.email}).from(verification).where(eq(verification.token, token));
 
-    return res[0].email ?? null;
+    return res[0]?.email ?? null;
 }
